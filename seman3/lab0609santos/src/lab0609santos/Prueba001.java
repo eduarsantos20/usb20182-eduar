cb3be78a27c146ec47f13b7b00da1ee07c8a715d
IqueMart <43053787+IqueMart@users.noreply.github.com>
2018-09-06 21:02:19 -0500

    Modificando001
    Modificando el aplicativo

---
+++ seman3/lab0609santos/src/lab0609santos/Prueba001.java
@@ -0,0 +1,22 @@
+
+package lab0609santos;
+
+public class Prueba001 {
+    String clase1,clase2;
+
+    public String getClase1() {
+        return clase1;
+    }
+
+    public void setClase1(String clase1) {
+        this.clase1 = clase1;
+    }
+
+    public String getClase2() {
+        return clase2;
+    }
+
+    public void setClase2(String clase2) {
+        this.clase2 = clase2;
+    }
+}
